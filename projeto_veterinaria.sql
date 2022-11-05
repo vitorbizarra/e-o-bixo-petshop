51b6793f2866f8493d4914880ed322709637d391
Vitor Bizarra <vitorbizarracontato@gmail.com>
2022-11-05 01:02:11 -0300

    Alteracao nome do banco de petshop para veterinaria :horse:

--- projeto_veterinaria.sql
+++ projeto_veterinaria.sql
@@ -1,22 +1,64 @@
-DROP DATABASE IF EXISTS projeto_petshop;
-CREATE DATABASE projeto_petshop;
-USE projeto_petshop;
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE animais(
-	ID 			INT PRIMARY KEY AUTO_INCREMENT,
-    Nome 		VARCHAR(40) NOT NULL,
-    Especie 	VARCHAR(40) NOT NULL,
-    Peso 		DECIMAL(10,2) NOT NULL CHECK(Peso > 0),
-    Cor 		VARCHAR(50) NOT NULL,
-    Data_nasc 	DATE NOT NULL
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE TABLE veterinario(
-	ID 			INT PRIMARY KEY AUTO_INCREMENT,
-    Nome		VARCHAR(100) NOT NULL,
-    Cpf			VARCHAR(14) NOT NULL,
-    Senha		VARCHAR(40) NOT NULL,
-    Data_nasc	DATE NOT NULL
-);
+-- -----------------------------------------------------
+-- Schema projeto_veterinaria
+-- -----------------------------------------------------
 
-INSERT INTO `veterinario`(`Nome`, `Cpf`, `Senha`, `Data_nasc`) VALUES ('Admin', '111.111.111-11', '21232f297a57a5a743894a0e4a801fc3', '1999-01-01');
+-- -----------------------------------------------------
+-- Schema projeto_veterinaria
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `projeto_veterinaria`;
+CREATE SCHEMA IF NOT EXISTS `projeto_veterinaria` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `projeto_veterinaria` ;
+
+-- -----------------------------------------------------
+-- Table `projeto_veterinaria`.`cliente`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `projeto_veterinaria`.`cliente` ;
+
+CREATE TABLE IF NOT EXISTS `projeto_veterinaria`.`cliente` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Nome` VARCHAR(40) NOT NULL,
+  `Endereco` VARCHAR(40) NOT NULL,
+  `Animal` VARCHAR(40) NOT NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `projeto_veterinaria`.`veterinario`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `projeto_veterinaria`.`veterinario` ;
+
+CREATE TABLE IF NOT EXISTS `projeto_veterinaria`.`veterinario` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Nome` VARCHAR(100) NOT NULL,
+  `Cpf` VARCHAR(14) NOT NULL,
+  `Senha` VARCHAR(40) NOT NULL,
+  `Data_nasc` DATE NOT NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `projeto_veterinaria`.`veterinario`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `projeto_veterinaria`;
+INSERT INTO `projeto_veterinaria`.`veterinario` (`ID`, `Nome`, `Cpf`, `Senha`, `Data_nasc`) VALUES (DEFAULT, 'Admin', '111.111.111-11', 'admin', '2000-01-01');
+
+COMMIT;
+
